dce36feab4cc9f3292a439b63106224cb81e1d4a
sputnick1124 <sputnick1124@comcast.net>
2016-04-07 01:07:15 -0400

    Change baud, lighter module import

--- scripts/waypoint.py
+++ scripts/waypoint.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
 import rospy
-from numpy import *
+from numpy import array, pi, abs
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from tf.transformations import euler_from_quaternion as efq
 
